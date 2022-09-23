2c230dc92e78c692e6f33fdf22d2e574ac6bc04e
john_dev <johnhaimez314@gmail.com>
2022-09-23 17:09:08 +0200

    add struct

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  ignorePatterns: ['*.json', '.eslintrc.js'],
+  ignorePatterns: ['*.json', '.eslintrc.js', '*.css'],
   plugins: [
     'react',
     '@typescript-eslint'
