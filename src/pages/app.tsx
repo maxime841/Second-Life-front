9341830ff8c1d000201fa39d210324d733449db6
maxime841 <maxime.poirot@gmail.com>
2022-09-27 16:16:19 +0200

    chore: idee de map sur router pour detecter le nombre de land en bdd

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,16 +1,23 @@
 import TemplatePublic from '@templates/template-public'
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './app.css'
 import ClubPage from './public/club-page'
 import HomePage from './public/home-page'
 import LoginPage from './public/login-page'
+import OneLand from './public/land-page'
+import React, { useState } from 'react'
 
 function App () {
+  const [lands] = useState([])
+  const routeLands = lands.map(({ path, Land }) =>
+    <Route key={Land} path={path} element={<OneLand />} />
+  )
+
   return (
     <Routes>
       <Route element={<TemplatePublic />}>
         <Route path='/' element={<HomePage />} />
+        {routeLands}
         <Route path='/login' element={<LoginPage />} />
         <Route path='/club' element={<ClubPage />} />
       </Route>
---
+++ src/pages/public/land-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Land () {
+  return (
+    <div>LandPage</div>
+  )
+}
+
+export default Land
