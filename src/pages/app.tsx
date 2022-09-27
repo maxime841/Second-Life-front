86bc21d7f1680dc79749d3cf07cee2f68fd32ad7
john_dev <johnhaimez314@gmail.com>
2022-09-27 19:00:06 +0200

    chore #3 : refactore router

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -4,20 +4,13 @@ import './app.css'
 import ClubPage from './public/club-page'
 import HomePage from './public/home-page'
 import LoginPage from './public/login-page'
-import OneLand from './public/land-page'
-import React, { useState } from 'react'
+import React from 'react'
 
-function App () {
-  const [lands] = useState([])
-  const routeLands = lands.map(({ path, Land }) =>
-    <Route key={Land} path={path} element={<OneLand />} />
-  )
-
+function App() {
   return (
     <Routes>
       <Route element={<TemplatePublic />}>
         <Route path='/' element={<HomePage />} />
-        {routeLands}
         <Route path='/login' element={<LoginPage />} />
         <Route path='/club' element={<ClubPage />} />
       </Route>
