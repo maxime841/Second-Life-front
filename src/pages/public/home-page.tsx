f9457dea57d92a2302f16501584096a8ff4b584d
maxime841 <maxime.poirot@gmail.com>
2022-10-08 20:44:59 +0200

    fixes: #i-12 photo page home ok

--- src/pages/public/home-page.tsx
+++ src/pages/public/home-page.tsx
@@ -1,21 +1,21 @@
 import Paragraph from '@atoms/texts/paragraph'
 import Image from '@atoms/images/image'
-/* import logoSL from './images/second life.png'
-import valombreuse from 'images/domaine projet.jpg'
-import club from 'images/Photo club.jpg' */
+import logoSL from '../../assets/second life.png'
+import valombreuse from '../../assets/domaine projet.jpg'
+import club from '../../assets/Photo club.jpg'
 import React from 'react'
 
 function HomePage () {
   return (
     <section>
-      <span className='flex items-center justify-center'><Image src='' alt='logo second life' size='w-16 h-8 mr-4' />
+      <span className='flex items-center justify-center'><Image src={logoSL} alt='logo second life' size='w-16 h-8 mr-4' />
         <Paragraph>Bienvenue sur le site de gestion de location et de club second life</Paragraph></span>
       <div className={`
            flex justify-center items-center sm:flex-col mt-7 md:flex-row mt-16
           `}
       >
-        <Image src='' alt='logo second life' size='sm:w-64 h-32 my-8 md:w-64 h-40 mx-8 lg:w-80 h-48' />
-        <Image src='' alt='logo second life' size='sm:w-64 h-32 my-8 md:w-50 h-40 mx-8 lg:w-80 h-48' />
+        <Image src={valombreuse} alt='logo second life' size='sm:w-64 h-32 my-8 md:w-64 h-40 mx-8 lg:w-80 h-48' />
+        <Image src={club} alt='logo second life' size='sm:w-64 h-32 my-8 md:w-50 h-40 mx-8 lg:w-80 h-48' />
       </div>
     </section>
   )
