aad752c3c4c1b784edb754cd0b9ff7d37f60bfe8
maxime841 <maxime.poirot@gmail.com>
2022-10-12 11:54:02 +0200

    feat #47 ternaire nok a revoir

--- src/components/organisms/section-menu-side-left-profil/menu-side-left-profil.tsx
+++ src/components/organisms/section-menu-side-left-profil/menu-side-left-profil.tsx
@@ -2,6 +2,7 @@ import { Btn } from '@atoms/btns/btn'
 import { LinkPrimary } from '@atoms/links/link-primary/link-primary'
 import { Store } from '@store/store'
 import React from 'react'
+import users from '../../../assets/user-profil.jpg'
 import './menu-side-left-profil.css'
 
 export function SectionMenuSideLeftProfil() {
@@ -10,8 +11,20 @@ export function SectionMenuSideLeftProfil() {
 return (
     <nav className='menu-side-left-profil'>
       <ul>
-        <img src={user.avatar} alt='presentation avatar user' className='w-64 h-52 p-8 rounded-full' />
-        <Btn addClass='ml-5'>Ajouter ma photo</Btn>
+        {
+          user
+        ? <img src={user.avatar} alt='presentation avatar user' className='w-56 h-52 p-8 rounded-full' />
+        : !user
+        ? <img src={users} alt='presentation avatar user' className='w-56 h-52 p-8 rounded-full' />
+        : null
+        }
+        {
+        <LinkPrimary link='/user/profil'>Mes informations</LinkPrimary>
+         ? <Btn addClass='ml-5'>Ajouter ma photo</Btn>
+        : <LinkPrimary link='/user/profil/update'>Modifier mon compte</LinkPrimary>
+         ? <Btn addClass='ml-5'>Modifier ma photo</Btn>
+        : null
+        }
         <li className='mt-4'>
           <LinkPrimary link='/user/profil'>Mes informations</LinkPrimary>
         </li>
