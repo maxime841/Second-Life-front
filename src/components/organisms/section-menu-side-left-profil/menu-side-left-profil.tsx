71cd06ad699f31a91d4a7d36f3b536c2eb562a59
maxime841 <maxime.poirot@gmail.com>
2022-10-12 10:46:27 +0200

    feat: #43 modif css

--- src/components/organisms/section-menu-side-left-profil/menu-side-left-profil.tsx
+++ src/components/organisms/section-menu-side-left-profil/menu-side-left-profil.tsx
@@ -1,13 +1,18 @@
+import { Btn } from '@atoms/btns/btn'
 import { LinkPrimary } from '@atoms/links/link-primary/link-primary'
 import { Store } from '@store/store'
 import React from 'react'
+import './menu-side-left-profil.css'
+
 export function SectionMenuSideLeftProfil() {
     const user = Store.user.useUserCurrent()
+
 return (
-    <nav className='flex-none w-52 bg-fond-cards dark:bg-fond-dark-cards top-0 max-h-screen text-black'>
+    <nav className='menu-side-left-profil'>
       <ul>
-        <img src={user.avatar} alt='presentation avatar user' className='p-8 rounded-full' />
-        <li>
+        <img src={user.avatar} alt='presentation avatar user' className='w-64 h-52 p-8 rounded-full' />
+        <Btn addClass='ml-5'>Ajouter ma photo</Btn>
+        <li className='mt-4'>
           <LinkPrimary link='/user/profil'>Mes informations</LinkPrimary>
         </li>
         <li>
