f20989c5e26e5b755eb75b3f9ba0bdc4469a2550
john_dev <johnhaimez314@gmail.com>
2022-10-18 17:08:25 +0200

    chore : add state for input's display name and email

--- src/components/molecules/cards/card-profil/card-profil.tsx
+++ src/components/molecules/cards/card-profil/card-profil.tsx
@@ -6,44 +6,57 @@ import { VoletDetailProfilUpdate } from '@molecules/volet-detail-profil-update/v
 import { VoletDetailProfilDelete } from '@molecules/volet-profil-delete/volet-profil-delete'
 import { VoletDetailProfilPasswordUpdate } from '@molecules/volet-profil-password-update/volet-profil-password-update'
 import { Store } from '@store/store'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export function CardProfil() {
-  /* const [pseudo, setPseudo] = useState('')
-  const [email, setEmail] = useState('') */
+  const [pseudo, setPseudo] = useState('')
+  const [email, setEmail] = useState('')
   const user = Store.user.useUserCurrent()
-  console.log('mon user', user)
 
-    return (
-<article className='bg-fond-cards dark:bg-fond-dark-cards width sm:w-96 md:w-[400px] lg:w-[700px] mx-auto mt-12 border-2 border-black p-4 rounded-lg'>
-          <TitleState addClass='sm:text-2xl md:text-3xl lg:text-4xl'>Mes informations</TitleState>
-                {/* display avatar */}
-            <div className='my-16'>
-              <img
-                className='w-28 h-28 rounded-full mx-auto my-8 z-10'
-                src={`${
-                 user.picture?.id
-                  ? `http://127.0.0.1:8000/${user.picture.picture_url}`
-                  : 'https://ui-avatars.com/api/?background=0D8ABC&color=fff'
-                }`}
-                />
-                {/* form avatar */}
-              <FormUploadAvatar />
-              <article className='mx-auto border-2 border-black rounded-lg w-fit p-8'>
-              <LabelPrimary>Pseudo: </LabelPrimary>
-              {/* <InputFull addClass='w-fit text-center mb-4' colors='bg-fond-cards' value={user.name} placeholder={''} setValueInput={setPseudo} /> */}
-              <LabelPrimary>Email: </LabelPrimary>
-              {/* <InputFull addClass='w-fit text-center' colors='bg-fond-cards' value={user.email} placeholder={''} setValueInput={setEmail} /> */}
-              </article>
-            </div>
-            <nav className='flex justify-center'>
-            {/* volet detail profil update */}
-            <VoletDetailProfilUpdate userCurrent={user} />
-            {/* volet detail profil update */}
-            <VoletDetailProfilPasswordUpdate userCurrent={user} />
-            {/* volet detail profil update */}
-            <VoletDetailProfilDelete userCurrent={user} />
-            </nav>
-          </article>
-    )
+  return (
+    <article className='bg-fond-cards dark:bg-fond-dark-cards width sm:w-96 md:w-[400px] lg:w-[700px] mx-auto mt-12 border-2 border-black p-4 rounded-lg'>
+      <TitleState addClass='sm:text-2xl md:text-3xl lg:text-4xl'>
+        Mes informations
+      </TitleState>
+      {/* display avatar */}
+      <div className='my-16'>
+        <img
+          className='w-28 h-28 rounded-full mx-auto my-8 z-10'
+          src={`${
+            user.picture?.id
+              ? `http://127.0.0.1:8000/${user.picture.picture_url}`
+              : 'https://ui-avatars.com/api/?background=0D8ABC&color=fff'
+          }`}
+        />
+        {/* form avatar */}
+        <FormUploadAvatar />
+        <article className='mx-auto border-2 border-black rounded-lg w-fit p-8'>
+          <LabelPrimary>Pseudo: </LabelPrimary>
+          <InputFull
+            addClass='w-fit text-center mb-4'
+            colors='bg-fond-cards'
+            value={user.id ? user.name : ''}
+            placeholder={''}
+            setValueInput={setPseudo}
+          />
+          <LabelPrimary>Email: </LabelPrimary>
+          <InputFull
+            addClass='w-fit text-center'
+            colors='bg-fond-cards'
+            value={user.id ? user.email : ''}
+            placeholder={''}
+            setValueInput={setEmail}
+          />
+        </article>
+      </div>
+      <nav className='flex justify-center'>
+        {/* volet detail profil update */}
+        <VoletDetailProfilUpdate userCurrent={user} />
+        {/* volet detail profil update */}
+        <VoletDetailProfilPasswordUpdate userCurrent={user} />
+        {/* volet detail profil update */}
+        <VoletDetailProfilDelete userCurrent={user} />
+      </nav>
+    </article>
+  )
 }
